82d48e0adf667dda1dfe8dff82a4deff2056990d
emwinckler <55844234+emwinckler@users.noreply.github.com>
2021-02-28 14:28:46 -0600

    Added Team Journal

--- app/src/main/java/com/example/myapplication/MySessions/My_Sessions.java
+++ app/src/main/java/com/example/myapplication/MySessions/My_Sessions.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.myapplication.MySessions;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.myapplication.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Get_A_Tutor#newInstance} factory method to
+ * Use the {@link My_Sessions#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Get_A_Tutor extends Fragment {
+public class My_Sessions extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class Get_A_Tutor extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Get_A_Tutor() {
+    public My_Sessions() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class Get_A_Tutor extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Get_A_Tutor.
+     * @return A new instance of fragment My_Sessions.
      */
     // TODO: Rename and change types and number of parameters
-    public static Get_A_Tutor newInstance(String param1, String param2) {
-        Get_A_Tutor fragment = new Get_A_Tutor();
+    public static My_Sessions newInstance(String param1, String param2) {
+        My_Sessions fragment = new My_Sessions();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class Get_A_Tutor extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_get__a__tutor, container, false);
+        return inflater.inflate(R.layout.fragment_my__sessions, container, false);
     }
 }
