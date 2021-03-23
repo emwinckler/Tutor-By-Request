a976f5c8931028fabe71c0f13e8afbab5c2c5ad4
Ryan Fries <rfries@wisc.edu>
2021-03-22 21:04:16 -0500

    created getter and setters for sessions

--- app/src/main/java/com/example/myapplication/Models/Session.java
+++ app/src/main/java/com/example/myapplication/Models/Session.java
@@ -29,4 +29,55 @@ public class Session {
         this.description = description;
         this.sessionID = sessionID;
     }
+
+    // Getters for the model
+    public String getStudentID(){ return this.studentID; }
+    public String getTutorID(){ return this.tutorID; }
+    public String getSessionID(){ return this.sessionID; }
+    public Date getDate(){ return this.date;}
+    public String getTime() { return time; }
+    public String getSubject() { return subject; }
+    public int getCourseNo() { return courseNo; }
+    public String getLocation() { return location; }
+    public String getDescription() { return description; }
+
+    // Setters for the model
+    // This needs to update the database
+
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public void setTutorID(String tutorID) {
+        this.tutorID = tutorID;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setCourseNo(int courseNo) {
+        this.courseNo = courseNo;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setSessionID(String sessionID) {
+        this.sessionID = sessionID;
+    }
 }
